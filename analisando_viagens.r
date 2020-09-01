f0a682ec2cb6d6d8e07d2dc39ef8bc39e83f1445
MaksonViini <maksonvinicio7@gmail.com>
2020-09-01 10:40:56 -0300

    Update analisando_viagens.r

--- analisando_viagens.r
+++ analisando_viagens.r
@@ -55,9 +55,25 @@ head(p1)
 library(ggplot2)
 ggplot(p1, aes(x=reorder(Orgao, Valor), y=Valor)) + geom_bar(stat='identity') + coord_flip() + labs(x='Valor', y='Orgao')
 
+# Quantidade de gastos por cidades com viagens
 p2 = viagens %>% group_by(Destinos) %>% summarise(n=sum(Valor.passagens)) %>% arrange(desc(n)) %>% top_n(15)
 names(p2) = c('Destino', 'Valor')
 head(p2)
 
 ggplot(p2, aes(x=reorder(Destino, Valor), y=Valor)) + geom_bar(stat='identity', fill='#0ba791') + geom_text(aes(label=Valor), 
 vjust=0.3, size=3) + coord_flip() + labs(x='Valor', y='Destino')
+
+options(scipen=999)
+
+# Quantidades de viagens realizadas pro mes
+p3 = viagens %>% group_by(data.inicio.formatada) %>% summarise(qtd=n_distinct(Identificador.do.processo.de.viagem))
+head(p3)
+
+ggplot(p3, aes(x=data.inicio.formatada, y=qtd, group=1)) + geom_line() + geom_point()
+
+
+# Visualizacao dos dados com Markdown
+install.packages('rmarkdown')
+install.packages('tinytex')
+
+tinytex::install_tinytex()
