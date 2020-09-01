1391762f86d3b9b338d46930b20c15e3711966f8
MaksonViini <maksonvinicio7@gmail.com>
2020-09-01 09:25:43 -0300

    Update analisando_viagens.r

--- analisando_viagens.r
+++ analisando_viagens.r
@@ -10,3 +10,25 @@ summary(viagens$valor.passagens)
 # Verificando o tipo de dados
 library(dplyr)
 glimpse(viagens)
+
+# Converter para o tipo data
+viagens$data.inicio = as.Date(viagens$Período...Data.de.início, '%d/ %m/ %y')
+glimpse(viagens)
+
+# Formatando o campo data para trabalhar apenas com mes e ano
+viagens$data.inicio.formatada = format(viagens$data.inicio, '%Y-%m')
+head(viagens$data.inicio.formatada)
+
+# Explorando os dados
+
+hist(viagens$valor.passagens)
+
+# Valores max, min e media .... da coluna valor
+summary(viagens$valor.passagens)
+
+# Visualizando os valores em boxplot
+boxplot(viagens$valor.passagens)
+
+# Desvio padrao
+
+sd(viagens$valor.passagens)
