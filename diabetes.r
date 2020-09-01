92f1149a7dc34356b33f9457e13d92f7f3bb8182
MaksonViini <maksonvinicio7@gmail.com>
2020-09-01 10:50:11 -0300

    Update diabetes.r

--- diabetes.r
+++ diabetes.r
@@ -27,3 +27,27 @@ boxplot(diabetes2$Insulin)
 
 # Construcao do modelo
 install.packages('caTools')
+library(caTools) # Pacote para dividir entre treino e teste
+
+set.seed(123)
+index = sample.split(diabetes2$Pregnancies, SplitRatio = .70) # Dividindo o pacote entre treino e teste
+index
+
+# Criando novos conjuntos de dados Train e Test
+train = subset(diabetes2, index == TRUE)
+test = subset(diabetes2, index == FALSE)
+
+# Verificando as dimensoes dos datasets
+dim(diabetes2)
+dim(train)
+dim(test)
+
+# Instalando pacote para criacao do modelo
+install.packages('caret')
+install.packages('e1071')
+
+# Carregando os pacotes
+library(caret)
+library(e1071)
+
+modelo = train(Outcome ~., data = train, method = 'knn')
