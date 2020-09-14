a4ef19a17e254f5d3dffa79e9942c981dc1c8641
MaksonViini <maksonvinicio7@gmail.com>
2020-09-14 19:00:27 -0300

    Update mongodb_com_python.py

--- testes/mongodb_com_python.py
+++ testes/mongodb_com_python.py
@@ -1,3 +1,11 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 print(client.list_database_names())
+
+# Cliente basse de dados
+db = client.Estudo_MongoDB
+
+# Inserindo dados
+db.teste.insert_many(
+    
+)
