ff1c9f988e9cbdbd3e6a7f91c77fc797b05008d4
MaksonViini <maksonvinicio7@gmail.com>
2020-09-14 19:25:49 -0300

    Update crud_com_mongodb.py

--- crud_com_mongodb.py
+++ crud_com_mongodb.py
@@ -7,4 +7,37 @@ print(client.list_database_names())
 
 db = client.Estudo_MongoDB
 
-db.insert_one()
+""" db.pessoas.insert_one(
+    {
+        'id': 1,
+        'Nome': 'Makson', 
+        'idade': 20
+    }
+) """
+
+"""db.pessoas.insert_one(
+    {
+        'id': 1,
+        'Nome': 'Aderbardo', 
+        'idade': 50, 
+        'filhos': ['Jr', 'Caboclo', 'Raules']
+
+    }
+) """
+
+db.pessoas.insert_many(
+    [
+        {
+        'id': 1,
+        'Nome': 'Aderbardo'
+        }, 
+        {
+        'id': 2,
+        'Nome': 'Canhoto'
+        }, 
+        {
+        'id': 3,
+        'Nome': 'Geraldao'
+        }
+    ]
+)
