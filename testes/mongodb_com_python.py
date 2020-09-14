e451f080d4cd3cb4c78b8168490260205c0eb455
MaksonViini <maksonvinicio7@gmail.com>
2020-09-14 19:05:24 -0300

    Update mongodb_com_python.py

--- testes/mongodb_com_python.py
+++ testes/mongodb_com_python.py
@@ -6,6 +6,14 @@ print(client.list_database_names())
 db = client.Estudo_MongoDB
 
 # Inserindo dados
-db.teste.insert_many(
-    
+"""db.teste.insert_many(
+    [
+        {'id': 1},
+        {'id': 12},
+        {'id': 500},
+        {'id': -222}
+    ]
+)"""
+db.teste.insert_one(
+    {'bala': 'Ze do amendoin'}
 )
