0a82754bba03d8a7dcefd5d845f3349ab4c68c9c
MaksonViini <maksonvinicio7@gmail.com>
2020-09-14 21:06:17 -0300

    commit

--- testes/mongodb_com_python.py
+++ testes/mongodb_com_python.py
@@ -17,3 +17,48 @@ db = client.Estudo_MongoDB
 db.teste.insert_one(
     {'bala': 'Ze do amendoin'}
 )
+
+
+""" comandos para inserir dados
+insert_one() e insert_many()
+"""
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+print(client.list_database_names())
+
+db = client.Estudo_MongoDB
+
+""" db.pessoas.insert_one(
+    {
+        'id': 1,
+        'Nome': 'Makson', 
+        'idade': 20
+    }
+) """
+
+"""db.pessoas.insert_one(
+    {
+        'id': 1,
+        'Nome': 'Aderbardo', 
+        'idade': 50, 
+        'filhos': ['Jr', 'Caboclo', 'Raules']
+
+    }
+) """
+
+db.pessoas.insert_many(
+    [
+        {
+        'id': 1,
+        'Nome': 'Aderbardo'
+        }, 
+        {
+        'id': 2,
+        'Nome': 'Canhoto'
+        }, 
+        {
+        'id': 3,
+        'Nome': 'Geraldao'
+        }
+    ]
+)
