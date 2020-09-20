690eae1a1022dfc4c40426b566ec265aec164770
MaksonViini <maksonvinicio7@gmail.com>
2020-09-19 21:11:07 -0300

    Update crud_com_mongodb.py

--- crud_com_mongodb.py
+++ crud_com_mongodb.py
@@ -2,3 +2,29 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 print(client.list_database_names())
 
+db = client.Estudo_MongoDB
+
+db.banco.insert_one(
+    {
+    'Title': {
+        'Titulo': 'TEste'
+    },
+    'Sub_Title': {
+        'type': 'String'
+    },
+    'Peoples': {
+        'Id': {
+            'type': 'String', 
+            'Nome': 'String',
+            'Cargo': 'String',
+            'Imagem': 'http/smdskdksd',
+            'Twitter': 'twitter.com',
+            'Facebook': 'fb.cmo',
+            'Gmail': 'gmail.com',
+            'linkedin': 'linkedin.com',
+            'unique': 'true'
+
+        }
+    } 
+    }
+) 
