842993d70442f2ad6375d41d71c424e513ac6c7c
MaksonViini <maksonvinicio7@gmail.com>
2020-09-27 12:21:26 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -3,29 +3,3 @@ client = MongoClient('localhost', 27017)
 print(client.list_database_names())
 titulo = str(input('Digite um titulo: '))
 db = client.Estudo_MongoDB
-
-
-
-db.createCollection({   
-    Title: {
-        type: String,
-    },
-    Sub_Title: {
-        type: String
-    },
-    Peoples: {
-        type: String,
-        Id: {
-            type: String, 
-            Nome: String,
-            Cargo: String,
-            Imagem: String,
-            Twitter: String,
-            Facebook: String,
-            Gmail: String,
-            linkedin: String,
-            unique: true
-
-        }
-    }             
-});
