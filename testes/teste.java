fe44db0610d9b14c0413fbd872c4f6b81c784f14
MaksonViini <maksonvinicio7@gmail.com>
2020-09-27 12:21:29 -0300

    Update teste.java

--- testes/teste.java
+++ testes/teste.java
@@ -1,6 +1,25 @@
 public class teste {
-    public static void main(String[] args) {
-         System.out.println("Hello World");
-         System.out.println("Ola mundo");
-    } 
+    public static void main(final String[] args) {
+        final int n1 = 6;
+        final int n2 = 8;
+        if (n1 < n2) {
+            System.out.println("n2 maior");
+            System.out.println(n2);
+        } else {
+            System.out.println("n1 maior");
+            System.out.println(n1);
+        }
+        
+        for (int i=0; i<5; i++) {
+            if (i == 2) {
+                continue;
+            }
+            
+            if (i == 4) {
+                break;
+            }
+            System.out.println(i);
+    }
+
+}
 }
