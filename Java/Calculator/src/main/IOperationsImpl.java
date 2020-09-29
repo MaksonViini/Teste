cd6c724a5a94f0547b0069c6682df28a3b9c7136
MaksonViini <maksonvinicio7@gmail.com>
2020-09-28 21:48:51 -0300

    Create IOperationsImpl.java

---
+++ Java/Calculator/src/main/IOperationsImpl.java
@@ -0,0 +1,13 @@
+package main;
+
+public interface IOperationsImpl {
+    public double add();
+    public double subt();
+    public double times();
+    public double divide();
+    public double potencia();
+    public double quadrado();
+    public double raiz();
+    public void reset();
+
+}
