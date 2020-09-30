1998ad65a51216e3be174ccc4ee970d74899a3cb
MaksonViini <maksonvinicio7@gmail.com>
2020-09-29 22:23:49 -0300

    Update script.py

--- testes/script.py
+++ testes/script.py
@@ -2,16 +2,18 @@
 from random import randint
 from time import sleep
 i = randint(0, 22)
-temas = ['Mobilidade urbana sustentável', 'A evasão escolar em questão no Brasil', 'A preservação da saúde mental no mundo globalizado',
+temas = ['Mobilidade urbana sustentável', 'A evasão escolar em questão no Brasil',
 'O meio ambiente em questão no Brasil', 'O perigo da crise hídrica no Brasil', 'Educação à distância', 
 'O aumento da expectativa de vida entre os brasileiros', 'Analfabetismo total e funcional', 
 'O trabalho na construção da dignidade humana', 'Viver em rede no século 21: os limites entre o público e o privado', 
 'Efeitos da implantação da Lei Seca no Brasil', 'Publicidade infantil em questão no Brasil', 
 'A persistência da violência contra a mulher na sociedade brasileira',
-'Valorização do SUS', 'Os impactos das fake news na sociedade brasileira', 'O acesso à informação na sociedade brasileira', 
+ 'Os impactos das fake news na sociedade brasileira', 'O acesso à informação na sociedade brasileira', 
 'Os impactos psicológicos do uso das redes sociais pelos jovens', 'Saúde nutricional no Brasil em debate', 
 'O envelhecimento e os direitos da população idosa no Brasil', 'A importância e os desafios da oferta de educação financeira nas escolas do Brasil', 
-'Homeschooling - vantagens e desvantagens da educação à distância para a formação básica'
+'Homeschooling - vantagens e desvantagens da educação à distância para a formação básica', 'Os perigos da automedicacao no Brasil', 
+'Perigos da resistencia a vacina',
+'Perigos do lixo eletronico para o meio ambiente', 'Desafios para a inclusao de refugiados na sociedade brasileira'
 ]
 sleep(1)
 print('BOA')
