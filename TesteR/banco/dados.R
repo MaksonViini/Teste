fb5688c7573ab4c573be58043b5ecad83b3098f2
MaksonViini <maksonvinicio7@gmail.com>
2020-10-31 19:47:04 -0300

    Update dados.R

--- TesteR/banco/dados.R
+++ TesteR/banco/dados.R
@@ -1,35 +1,37 @@
 library("DBI")
 library(leaflet)
 
-#con = dbConnect(RMySQL::MySQL(),
- #              dbname = 'painel',
-  #              host = '127.0.0.1',
-  #             user = 'root',
-   #             password = ''
-#)
+con = dbConnect(RMySQL::MySQL(),
+              dbname = 'teste',
+               host = 'localhost',
+              user = 'root',
+               password = ''
+)
 
-#dbListTables(con)
+dbListTables(con)
 
-df = data.frame(lat = runif(20, min = -19.81, max = -19.8), 
-                lng = runif(20, min = -43.95, max = -43.9))
+df = data.frame(lat = runif(40, min = -19.8157, max = -19.80), 
+                lng = runif(40, min = -43.9542, max = -43.94))
 
-states = geojsonio::geojson_read("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", what = "sp")
+#states = geojsonio::geojson_read("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json", what = "sp")
 
-class(states)
+df2 = dbGetQuery(con, "select lat, lng from mapa")
 
-names(states)
 
-head(df)
-lat = -19.8157
-lng = -43.9542
+#head(df)
+df2
+#lat = -19.8157
+#lng = -43.9542
 
-df %>% leaflet() %>% addTiles() %>% 
-  addCircleMarkers(clusterOptions = markerClusterOptions())
+
+df2 %>% leaflet() %>% addTiles() %>% 
+  addCircleMarkers(clusterOptions = markerClusterOptions()) %>% 
+  setView(df2, lat = -19.8157, lng = -43.9542, zoom=12)
+  
 
   
 
 
-#	Latitude: -19.8157 Longitude: -43.9542
 #my_map = leaflet() %>% addTiles()
 #my_map = my_map %>% addMarkers(lat=vetor_lat, lng=vetor_long, popup = 'Teste')
 #my_map
