0c6de4e8d70c211901fdc7bb7dbd4bc7219eba4e
MaksonViini <maksonvinicio7@gmail.com>
2020-11-12 18:41:54 -0300

    Create MyArithmeticException.java

---
+++ Java/Calculator/src/main/MyArithmeticException.java
@@ -0,0 +1,19 @@
+package main; 
+
+  
+
+public class MyArithmeticException extends Exception { 
+
+ 
+
+    private static final long serialVersionUID = 1L; 
+
+  
+
+    public MyArithmeticException(String excepString) { 
+
+        super (excepString); 
+
+    } 
+
+} 
