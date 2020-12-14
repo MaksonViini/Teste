cad7705315d7de3ea8b0974c3ad32123e4761481
MaksonViini <maksonvinicio7@gmail.com>
2020-12-13 22:09:46 -0300

    Create lambda.py

---
+++ pyclasse/lambda.py
@@ -0,0 +1,10 @@
+from math import sqrt
+soma = lambda a,b: a + b
+mult = lambda a,b: a * b
+div = lambda a,b: a / b
+raiz = lambda a: sqrt(a)
+
+print(soma(1, 3))
+print(mult(1, 3))
+print(div(1, 3))
+print(raiz(9))
