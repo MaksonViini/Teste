6f6c431d33e8ddcffcb4d311c7dc038b4661bf91
MaksonViini <maksonvinicio7@gmail.com>
2020-12-19 12:20:57 -0300

    Create certificado.py

---
+++ certificados/certificado.py
@@ -0,0 +1,31 @@
+from xlrd import open_workbook
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+
+def gera_certificado(nome_certificado, nome_arquivo):
+    nome_do_arquivo_certificado = 'certificado_' + nome_arquivo.replace(' ', '_') + '.png'
+    imagem_certificado = Image.open('certificados\\cet.png')
+    imagem_certificado_desenho = ImageDraw.Draw(imagem_certificado)
+    fonte_certificado = ImageFont.truetype("arial.ttf", 25)
+    fonte = ImageFont.truetype("arial.ttf", 15)
+
+    text = "Escrevendo um texto, para testar o certificado, \nEmitido no dia 17/12/2020, na capacitacao \
+    \nTeste X Wordpress com Python test H"
+
+    imagem_certificado_desenho.text((400, 300), nome_certificado, font=fonte_certificado, fill=(0, 0, 0))
+    imagem_certificado_desenho.text((400, 350), text, font=fonte, fill=(0, 0, 0))
+    imagem_certificado.save(nome_do_arquivo_certificado, 'PNG', resolution=100.0)
+    return nome_do_arquivo_certificado
+
+if __name__ == '__main__':
+
+    planilha_inscricoes = open_workbook('certificados\\students.xlsx')
+    folha_planilha_inscricoes = planilha_inscricoes.sheet_by_index(0)
+
+
+    for i in range(1, folha_planilha_inscricoes.nrows):
+        aluno_nome_certificado = folha_planilha_inscricoes.row_values(i)[3]
+        aluno_nome_arquivo = folha_planilha_inscricoes.row_values(i)[2]
+
+        nome_certificado = gera_certificado(aluno_nome_certificado, aluno_nome_arquivo)
