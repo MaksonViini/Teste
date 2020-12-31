cef3e518d8b2e04047b7c9cc09b19fa763e3454e
MaksonViini <maksonvinicio7@gmail.com>
2020-12-31 14:43:47 -0300

    Create lista_ordenada.py

---
+++ coursera/lista_ordenada.py
@@ -0,0 +1,12 @@
+def ordenada(lista):
+    valor = lista[0]
+    for i in range(len(lista)):
+        if valor <= lista[i]:
+            valor = lista[i]
+        else:
+            return False
+    return True
+
+lista = [1, 5, 8]
+
+print(ordenada(lista))
