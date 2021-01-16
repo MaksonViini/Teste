f4bb418a1943bb2d33dbd061de8bf22618740410
MaksonViini <maksonvinicio7@gmail.com>
2021-01-16 19:56:49 -0300

    nem

---
+++ coursera/elefantes.py
@@ -0,0 +1,11 @@
+def elefantes(n):
+    if n <= 0: return ""
+    if n == 1: return "Um elefante incomoda muita gente"
+    return elefantes(n - 1) + str(n) + " elefantes " + incomodam(n) + ("muita gente" if n % 2 > 0 else "muito mais") + "\r\n"
+
+def incomodam(n):
+    if n <= 0: return ""
+    if n == 1: return "incomodam "
+    return "incomodam " + incomodam(n - 1)
+
+print(elefantes(4))
