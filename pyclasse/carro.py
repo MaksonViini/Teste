768f2d9fcad7d0c08e07ca95e1b72d75a0ecc7ac
MaksonViini <maksonvinicio7@gmail.com>
2021-01-31 14:25:28 -0300

    mudanca

--- pyclasse/carro.py
+++ pyclasse/carro.py
@@ -1,3 +1,14 @@
 class Carro:
-    def __init__(self, marca):
-        self.
+    def __init__(self, vel_max, ligado, cor):
+        self.vel_max = vel_max
+        self.ligado = ligado
+        self.cor = cor
+    
+    def mostra_atributos(self):
+        print(self.vel_max)
+        print(self.ligado)
+        print(self.cor)
+
+c1 = Carro(300, True, 'Azul')
+
+c1.mostra_atributos()
