081e40839c0ec3d8ed7db4e6e54688dde77a45a0
MaksonViini <maksonvinicio7@gmail.com>
2021-01-31 15:17:37 -0300

    'poo'

--- pyclasse/heranca.py
+++ pyclasse/heranca.py
@@ -4,6 +4,35 @@ class NPC:
         self.time = time
         self.forca = forca
         self.municao = municao
+        self.vivo = True
+        self.energia = 100
     
-    
+    def info(self):
+        print('Nome' + self.nome)
+        print('Time' + str(self.time))
+        print('Forca' + str(self.forca))
+        print('Municao' + str(self.municao))
+        print('Vivo' + self.vivo)
+        print('Energia' + str(self.energia))
+
+class Soldado(NPC):
+    def __init__(self, nome, time):
+        self.forca = 200
+        self.municao = 200
+        super().__init__(nome, time, self.forca, self.municao)
+
+class Guarda(NPC):
+    def __init__(self, nome, time):
+        self.forca = 100
+        self.municao = 20
+        super().__init__(nome, time, self.forca, self.municao)
+
+class Elite(NPC):
+    def __init__(self, nome, time):
+        self.forca = 400
+        self.municao = 500
+        super().__init__(nome, time, self.forca, self.municao)
+        
+        def inf(self):
+            super().info()
         
