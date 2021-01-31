748b102472157b872687fcf31cf7904c6f7b6876
MaksonViini <maksonvinicio7@gmail.com>
2021-01-31 14:29:57 -0300

    'alter'

--- pyclasse/carro.py
+++ pyclasse/carro.py
@@ -9,6 +9,15 @@ class Carro:
         print(self.ligado)
         print(self.cor)
 
+    def altera_atributos(self, vel_max=300, ligado=True, cor='Azul'):
+        self.vel_max = vel_max
+        self.ligado = ligado
+        self.cor = cor
+
 c1 = Carro(300, True, 'Azul')
+
+c1.mostra_atributos()
+
+c1.altera_atributos(1, False)
 
 c1.mostra_atributos()
