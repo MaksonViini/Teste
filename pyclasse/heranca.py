d71f4950c5040045904d0ac21a187092016fd930
MaksonViini <maksonvinicio7@gmail.com>
2021-02-01 21:15:50 -0300

    Update heranca.py

--- pyclasse/heranca.py
+++ pyclasse/heranca.py
@@ -8,12 +8,13 @@ class NPC:
         self.energia = 100
     
     def info(self):
-        print('Nome' + self.nome)
-        print('Time' + str(self.time))
-        print('Forca' + str(self.forca))
-        print('Municao' + str(self.municao))
-        print('Vivo' + self.vivo)
-        print('Energia' + str(self.energia))
+        print('Nome: ' + self.nome)
+        print('Time: ' + str(self.time))
+        print('Forca: ' + str(self.forca))
+        print('Municao: ' + str(self.municao))
+        print('Vivo: ' + str(self.vivo))
+        print('Energia: ' + str(self.energia))
+        print('-' * 15)
 
 class Soldado(NPC):
     def __init__(self, nome, time):
@@ -33,6 +34,21 @@ class Elite(NPC):
         self.municao = 500
         super().__init__(nome, time, self.forca, self.municao)
         
-        def inf(self):
-            super().info()
+    def inf(self):
+        super().info()
         
+s1 = Guarda('Braum', 1)
+s2 = Guarda('Vlad', 1)
+s3 = Elite('Yone', 1)
+s4 = Guarda('Leon', 2)
+s5 = Soldado('Darius', 2)
+s6 = Elite('Jax', 2)
+
+s1.vivo = False
+
+s1.info()
+s2.info()
+s3.info()
+s4.info()
+s5.info()
+s6.inf()
