3eb1589763eb184389eaef6452491abd3879caef
MaksonViini <maksonvinicio7@gmail.com>
2021-02-01 21:16:00 -0300

    Create mongodb.js

---
+++ testes/mongodb.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const UserSchema  = new mongoose.Schema({   
+    Title: {
+        type: String,
+    },
+    Sub_Title: {
+        type: String
+    },
+    Peoples: {
+        type: String,
+        Id: {
+            type: String, 
+            Nome: String,
+            Cargo: String,
+            Imagem: String,
+            Twitter: String,
+            Facebook: String,
+            Gmail: String,
+            linkedin: String,
+            unique: true
+
+        }
+    }             
+});
+
+mongoose.model('test', UserSchema);
