9885650aa24d8adb5d04a64b92ed81d22626529b
MaksonViini <maksonvinicio7@gmail.com>
2021-03-12 19:50:26 -0300

    Formatando a estrutura

--- testes/mongodb_com_python.py
+++ testes/mongodb_com_python.py
@@ -22,7 +22,6 @@ db.teste.insert_one(
 """ comandos para inserir dados
 insert_one() e insert_many()
 """
-from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 print(client.list_database_names())
 
@@ -49,16 +48,16 @@ db = client.Estudo_MongoDB
 db.pessoas.insert_many(
     [
         {
-        'id': 1,
-        'Nome': 'Aderbardo'
-        }, 
+            'id': 1,
+            'Nome': 'Aderbardo'
+        },
         {
-        'id': 2,
-        'Nome': 'Canhoto'
-        }, 
+            'id': 2,
+            'Nome': 'Canhoto'
+        },
         {
-        'id': 3,
-        'Nome': 'Geraldao'
+            'id': 3,
+            'Nome': 'Geraldao'
         }
     ]
 )
--- testes/py_mongo.py
+++ testes/py_mongo.py
@@ -6,24 +6,24 @@ db = client.Estudo_MongoDB
 
 db.banco.insert_one(
     {
-    'Title': {
-        'Titulo': titulo,
-    },
-    'Sub_Title': {
-        'type': 'String'
-    },
-    'Peoples': {
-        'Id': {
-            'type': 'String', 
-            'Nome': 'String',
-            'Cargo': 'String',
-            'Imagem': 'http/smdskdksd',
-            'Twitter': 'twitter.com',
-            'Facebook': 'fb.cmo',
-            'Gmail': 'gmail.com',
-            'linkedin': 'linkedin.com',
-            'unique': 'true'
+        'Title': {
+            'Titulo': titulo,
+        },
+        'Sub_Title': {
+            'type': 'String'
+        },
+        'Peoples': {
+            'Id': {
+                'type': 'String',
+                'Nome': 'String',
+                'Cargo': 'String',
+                'Imagem': 'http/smdskdksd',
+                'Twitter': 'twitter.com',
+                'Facebook': 'fb.cmo',
+                'Gmail': 'gmail.com',
+                'linkedin': 'linkedin.com',
+                'unique': 'true'
+            }
         }
-    } 
     }
-) 
+)
