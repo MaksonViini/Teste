1c8cb77473f116836efe84c2ecfbe77e7392033f
MaksonViini <maksonvinicio7@gmail.com>
2021-03-13 22:17:18 -0300

    Adicionando programa para usar funcoes lambda

---
+++ DIO/POO/aula08_lambda.py
@@ -0,0 +1,9 @@
+contador = lambda lista: [len(x) for x in lista]
+
+soma = lambda a, b : a + b
+mult = lambda a, b : a * b
+sub  = lambda a, b : a - b
+div  = lambda a, b : a / b
+
+
+print(contador(['pala']))
