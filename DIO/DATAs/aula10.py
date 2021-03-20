652f7ab970ff31aed2bc2334216ed24205682169
MaksonViini <maksonvinicio7@gmail.com>
2021-03-20 19:40:31 -0300

    Transformando em metodos com a classe Dates

--- DIO/DATAs/aula10.py
+++ DIO/DATAs/aula10.py
@@ -1,5 +1,21 @@
-from datetime import date
+from datetime import date, time
 
-data_atual = date.today()
+class Dates:
+    def trabalhando_com_date():
+        data_atual = date.today()
 
-print(data_atual.strftime('%d/%m/%y'))
+        # Formatando data para dia mes ano
+        print(data_atual.strftime('%d/%m/%y'))
+
+        # Formatando data para dia, mes e ano na forma escrita
+        print(data_atual.strftime('%A %B %Y')) 
+
+
+    def trabalhando_com_time():
+        horario = time(hour=15, minute=18, second=20)
+
+
+# if __name__ == '__main__':
+dates = Dates
+dates.trabalhando_com_date()
+dates.trabalhando_com_time()
