e43d994eef76a8bc6df966237b8599905545c51c
MaksonViini <maksonvinicio7@gmail.com>
2021-03-20 19:45:39 -0300

    Manipulando datetime

--- DIO/DATAs/aula10.py
+++ DIO/DATAs/aula10.py
@@ -1,21 +1,29 @@
-from datetime import date, time
+from datetime import date, time, datetime
+
 
 class Dates:
     def trabalhando_com_date():
         data_atual = date.today()
 
         # Formatando data para dia mes ano
-        print(data_atual.strftime('%d/%m/%y'))
+        print(data_atual.strftime('%d/%m/%y\n'))
 
         # Formatando data para dia, mes e ano na forma escrita
-        print(data_atual.strftime('%A %B %Y')) 
-
+        print(data_atual.strftime('%A %B %Y\n'))
 
     def trabalhando_com_time():
         horario = time(hour=15, minute=18, second=20)
+        print(horario.strftime('%H:%M:%S\n'))
+
+
+    def trabalhando_com_datetime():
+        data_hora = datetime.today()
+        print(data_hora, '\n')
+        print(data_hora.strftime('%d/%m/%y %H:%M:%S\n'))
 
 
 # if __name__ == '__main__':
 dates = Dates
 dates.trabalhando_com_date()
 dates.trabalhando_com_time()
+dates.trabalhando_com_datetime()
