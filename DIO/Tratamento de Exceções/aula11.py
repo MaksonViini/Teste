5e3e3fb8efbeeaeddb00b9f0f1315ac35d371cbb
MaksonViini <maksonvinicio7@gmail.com>
2021-03-30 21:42:58 -0300

    except

--- DIO/Tratamento de Exceções/aula11.py
+++ DIO/Tratamento de Exceções/aula11.py
@@ -3,12 +3,19 @@ lista = [1, 10]
 try:
     divisao = 10 / 1
     numero = lista[1]
-    x = a
+    # x = a
+
 except ZeroDivisionError:
     print('Nao e possivel realizar uma divisao por 0')
 
 except IndexError:
     print('Erro de Indice')
 
-except BaseException as ex:
+except Exception as ex:
     print('Erro desconhecido: Erro {}'.format(ex))
+
+else:
+    print('Nao ocorreu nenhuma excecao!!!')
+
+finally:
+    print('Sempre executa')
---
+++ DIO/Tratamento de Exceções/aula11_personaliza_excecao.py
@@ -0,0 +1,1 @@
+x = int(input('Entre com uma nota de 0 a 10: '))
