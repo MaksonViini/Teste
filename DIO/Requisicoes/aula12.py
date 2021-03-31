ec780bc0c9f66c438c5809e2d527011f71a82b57
MaksonViini <maksonvinicio7@gmail.com>
2021-03-31 20:35:27 -0300

    DIO aula 11, requests

--- DIO/Requisicoes/aula12.py
+++ DIO/Requisicoes/aula12.py
@@ -18,7 +18,15 @@ def return_data_pokemon(pokemon):
     return response.json()
 
 
+def retorn_response(url):
+    response = requests.get(url)
+
+    return response.text
+
+
 if __name__ == '__main__':
-    # return_data_cep(39775000)
-    dados_pokemon = return_data_pokemon('pikachu')
-    print(dados_pokemon['sprites']['front_shiny'])
+    return_data_cep(39775000)
+    # dados_pokemon = return_data_pokemon('pikachu')
+    # print(dados_pokemon['sprites']['front_shiny'])
+    # response = retorn_response('http://ww1.globallabs.academy/')
+    # print(response)
