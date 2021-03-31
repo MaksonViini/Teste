781ec2588756679206672c3b66158d9ec34fdaa6
MaksonViini <maksonvinicio7@gmail.com>
2021-03-30 21:55:48 -0300

    Personalizando a excecao'

--- DIO/Tratamento de Exceções/aula11_personaliza_excecao.py
+++ DIO/Tratamento de Exceções/aula11_personaliza_excecao.py
@@ -1,1 +1,27 @@
-x = int(input('Entre com uma nota de 0 a 10: '))
+class Error(Exception):
+    pass
+
+
+class InputError(Error):
+
+    def __init__(self, message):
+        self.messege = message
+
+
+while True:
+    try:
+        x = int(input('Entre com uma nota de 0 a 10: '))
+        if x > 10:
+            raise InputError('Nota nao pode ser maior que 10!')
+
+        elif x < 0:
+            raise InputError('Nota nao pode ser menor que 0!')
+
+        print(f'A nota digitada foi {x}')
+        break
+
+    except ValueError:
+        print('Valor invalido!')
+
+    except InputError as ex:
+        print(ex)
