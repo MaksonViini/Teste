a3a4a986faaa6a7d5f9f8cbaa8bf1ce4fc217cbc
MaksonViini <maksonvinicio7@gmail.com>
2021-03-31 20:29:31 -0300

    DIO aula 11, requests

---
+++ DIO/Requisicoes/aula12.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def return_data_cep(cep):
+    response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+
+    print(response.status_code)
+    print(response.json())
+
+    dados_cep = response.json()
+
+    print(dados_cep['localidade'])
+
+
+def return_data_pokemon(pokemon):
+    response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon}')
+
+    return response.json()
+
+
+if __name__ == '__main__':
+    # return_data_cep(39775000)
+    dados_pokemon = return_data_pokemon('pikachu')
+    print(dados_pokemon['sprites']['front_shiny'])
