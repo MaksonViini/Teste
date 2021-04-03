8f2a2a2c42bc3335be973606ceb7207bef9cd03f
MaksonViini <maksonvinicio7@gmail.com>
2021-04-02 23:23:29 -0300

    Crindo Rest API completa

--- DIO/RestApi com Flask/dev-api.py
+++ DIO/RestApi com Flask/dev-api.py
@@ -13,20 +13,58 @@ dev = [{
     'Habilidades': ['Python', 'Git']
 }]
 
+# Devolve uma desenvolvedor pelo ID, altera e deleta
 
-@app.route('/dev/<int:id>/', methods=['GET', 'PUT'])
+
+@app.route('/dev/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
 def developer(id):
     if request.method == 'GET':
-        desenvolvedor = dev[id]
-        return jsonify(
-            desenvolvedor
-        )
+        try:
+            response = dev[id]
+            return jsonify(
+                response
+            )
+        except IndexError:
+            response = {
+                'status': 'erro',
+                'Mensagem': f'Desenvolvedor de ID {id} nao existe'
+            }
+        except Exception:
+            response = {
+                'status': 'erro',
+                'Mensagem': f'Erro desconhecido, procure o adm da API'
+            }
+        return jsonify(response)
 
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         dev[id] = dados
         return jsonify(
             dados
+        )
+
+    elif request.method == 'DELETE':
+        dev.pop(id)
+        return jsonify(
+            {
+                'status': 'Sucess',
+                'Mensagem': 'Registro excluido!'
+            }
+        )
+
+# Lista todos os desenvolvedores e permite registrar um novo dev
+
+
+@app.route('/dev/', methods=['POST', 'GET'])
+def lista_dev():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        dev.append(dados)
+        return jsonify(
+            {
+                'status': 'Sucess',
+                'Mensagem': 'Registro Inserido!'
+            }
         )
 
 
