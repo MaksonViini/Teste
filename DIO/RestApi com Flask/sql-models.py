ac4fd332065070d6c1ae0f4f1e8105b8510c0d1a
MaksonViini <maksonvinicio7@gmail.com>
2021-04-06 10:52:37 -0300

    Criando tabelas com SQLAlchemy

--- DIO/RestApi com Flask/sql-models.py
+++ DIO/RestApi com Flask/sql-models.py
@@ -1,10 +1,34 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, Column, Interger, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import column
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer
 
 engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          binds=engine))
 
-base = declarative_base()
-base.query = db_session.query_property()
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+class Pessoas(Base):
+    __tablename__ = 'pessoas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return f'<Pessoa {self.nome}'
+
+
+class Atividades(Base):
+    __tablename__ = 'Atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
+    pessoa = relationship('Pessoas')
+
+    def init_db():
+        Base.metadata.create_all(bind=engine)
