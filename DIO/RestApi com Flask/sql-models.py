7bf43441f4f00ea1651beebb851f0236a80afa1b
MaksonViini <maksonvinicio7@gmail.com>
2021-04-06 10:54:15 -0300

    Criando database com SQLAlchemy

--- DIO/RestApi com Flask/sql-models.py
+++ DIO/RestApi com Flask/sql-models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Interger, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import column
