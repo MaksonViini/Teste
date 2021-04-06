4b60c4124c543a3321b2b5e1b406528ea736d1fa
MaksonViini <maksonvinicio7@gmail.com>
2021-04-06 10:45:58 -0300

    Trabalhando com SQLAlchemy

--- DIO/RestApi com Flask/sql-models.py
+++ DIO/RestApi com Flask/sql-models.py
@@ -1,5 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         binds=engine))
 
+base = declarative_base()
+base.query = db_session.query_property()
