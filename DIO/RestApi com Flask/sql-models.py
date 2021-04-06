66c0848989207302e6e84e63a49baa3ccf8cb847
MaksonViini <maksonvinicio7@gmail.com>
2021-04-06 10:53:14 -0300

    Criando tabelas com SQLAlchemy

--- DIO/RestApi com Flask/sql-models.py
+++ DIO/RestApi com Flask/sql-models.py
@@ -30,5 +30,10 @@ class Atividades(Base):
     pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
     pessoa = relationship('Pessoas')
 
-    def init_db():
-        Base.metadata.create_all(bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    init_db()
