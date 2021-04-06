097cd1dbcdea1fd61f293cdf41a77e122649bffd
MaksonViini <maksonvinicio7@gmail.com>
2021-04-06 12:06:57 -0300

    Criando a api tarefas, exercicio passado

---
+++ DIO/RestApi com Flask/tasks_api.py
@@ -0,0 +1,55 @@
+from flask import Flask, jsonify
+from flask.globals import request
+import json
+
+app = Flask(__name__)
+
+tarefas = [{
+    'id': '0',
+    'responsavel': 'Makson',
+    'tarefa': 'Desenvolver metodo GET',
+    'status': 'Concluido'
+},
+    {
+    'id': '1',
+    'responsavel': 'Vinicio',
+    'tarefa': 'Desenvolver metodo POST',
+    'status': 'Pendente'
+}]
+
+
+@app.route('/task/<int:id>/', methods=['GET', 'PUT', 'DELETE'])
+def task(id):
+    if request.method == 'GET':
+        try:
+            response = tarefas[id]
+            return jsonify(response)
+        except IndexError:
+            response = {
+                'status': 'erro',
+                'Mensagem': f'A tarefa de ID {id} nao existe'
+            }
+        except Exception:
+            response = {
+                'status': 'erro',
+                'Mensagem': f'Erro desconhecido, procure o adm da API'
+            }
+        return jsonify(response)
+
+    elif request.method == 'PUT':
+        dados = json.loads(request.data)
+        tarefas[id] = dados
+        return jsonify(dados)
+
+    elif request.method == 'DELETE':
+        tarefas.pop(id)
+        return jsonify(
+            {
+                'status': 'Sucess',
+                'Mensagem': 'Registro excluido!'
+            }
+        )
+
+
+if __name__ == '__main__':
+    app.run()
