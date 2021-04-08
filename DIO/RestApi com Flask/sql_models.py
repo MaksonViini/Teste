03712d10212b47415c998f4594b4a586dbc0e328
MaksonViini <maksonvinicio7@gmail.com>
2021-04-08 14:50:15 -0300

    Criando um sistema de autenticacao

--- DIO/RestApi com Flask/sql_models.py
+++ DIO/RestApi com Flask/sql_models.py
@@ -1,3 +1,4 @@
+from enum import unique
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -47,6 +48,24 @@ class Atividades(Base):
 
     def delete(self):
         db_session.delete(self)
+        db_session.commit()
+
+
+class Usuarios(Base):
+    __tablename__ = 'usuarios'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(20), unique=True)
+    senha = Column(String(20))
+
+    def __repr__(self):
+        return f'<Usuario {self.login}>'
+
+    def save(self):
+        db_session.add()
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete()
         db_session.commit()
 
 
