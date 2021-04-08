889f4d5b6c0ab446524fdf880bd13a25be3fa4df
MaksonViini <maksonvinicio7@gmail.com>
2021-04-08 10:26:38 -0300

    Trabalhando com flask e sqlalchemy

--- DIO/RestApi com Flask/app.py
+++ DIO/RestApi com Flask/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, jsonify
-from flask.globals import request
+from flask import Flask, request
+from flask_restful import Resource, Api
 import json
 
 app = Flask(__name__)
+api = Api(app)
+
+
+if __name__ == '__main__':
+    app.run()
