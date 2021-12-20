d8fc55d8d413b16fa3563ad424158fa60ff0642d
maksonviini <maksonvinicio7@gmail.com>
2021-12-20 18:37:49 -0300

    transformando em restful api

--- PyTest/Flask RestApi/app.py
+++ PyTest/Flask RestApi/app.py
@@ -1,17 +1,28 @@
 from flask import Flask, jsonify, request
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
+
+api = Api(app, version='1.0', title='TodoMVC API',
+    description='A simple TodoMVC API',
+    doc='/docs/'
+)
 
 data = {
     'id': 1,
     'value': 10
 }
 
+class Model(Resource):
+    def get(self):
+        return jsonify(data)
 
-@app.route('/', methods=['GET'])
-def index():
-    return jsonify(data)
+    def post(self):
+        data['id'] = request.json['id']
+        data['value'] = request.json['value']
+        return jsonify(data)
 
+api.add_resource(Model, '/model/')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- PyTest/Flask RestApi/test_app.py
+++ PyTest/Flask RestApi/test_app.py
@@ -7,6 +7,16 @@ def test_data():
     'value': 10
     }
 
-    response = requests.get('http://localhost:5000/')
+    response = requests.get('http://localhost:5000/model')
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_create():
+    data = {
+    'id': 1,
+    'value': 10
+    }
+
+    response = requests.post('http://localhost:5000/model', json=data)
     assert response.status_code == 200
     assert response.json() == data
