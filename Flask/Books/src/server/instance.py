df2559e2c91fc25a5692921ca2d6b599badb6c42
maksonviini <maksonvinicio7@gmail.com>
2021-12-21 10:10:55 -0300

    Instanciando serializacao e database

---
+++ Flask/Books/src/server/instance.py
@@ -0,0 +1,25 @@
+from flask import Flask, blueprints
+from flask_restx import Api
+
+app = Flask(__name__)
+
+
+class Server():
+    def __init__(self, ) -> None:
+        self.app = Flask(__name__)
+        self.blueprint = blueprints('api', __name__, url_prefix='/api')
+        self.api = Api(self.blueprint, doc='/docs', title='Api Flask-SQLAlchemy',
+                       version='1.0', description='API for the server')
+        self.app.register_blueprint(self.blueprint)
+
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+        self.app.config['PROPAGATE_EXCEPTIONS'] = True
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        self.book_name_space = self.book_name_space()
+
+    def book_name_space(self):
+        return self.api.namespace('book', description='Book operations', path='/')
+
+    def run(self, ):
+        self.app.run(debug=True, host='0.0.0.0', port=5000)
